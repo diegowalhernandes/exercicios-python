e2ae40c84a041205902a419a5459a2ec7f7ff922
Diego Hernandes <d.cunha.hernandes@avanade.com>
2025-01-19 17:22:12 -0300

    exercício soma

---
+++ ex002.py
@@ -0,0 +1,7 @@
+# 2. Soma de Dois Números
+# Peça dois números ao usuário, some-os e mostre o resultado.
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+print(f"A soma de {n1} + {n2} é = {n1 + n2}")
