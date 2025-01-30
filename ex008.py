609875a81735b3a92f0ac8d4993b423219684f66
Diego Hernandes <d.cunha.hernandes@avanade.com>
2025-01-30 08:21:27 -0300

    list functions

---
+++ ex008.py
@@ -0,0 +1,11 @@
+# Lis Functions
+
+# Given a list, calculate the middle element's index.
+
+# you can use the len() function to get the number of elements in the list and then floor divide it by 2
+# using two slashes (//)
+
+items = [2, 4, 6, 8, 10, 12, 14]
+middle = len(items)//2
+
+print(middle)
